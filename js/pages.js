67dddd70bce4deb4bc1eab0341df1b77604e5f65
Ariadna Victorero <aria@html24.net>
2021-05-28 11:28:43 +0200

    updated submitForm

--- js/pages.js
+++ js/pages.js
@@ -97,21 +97,28 @@ hideLoader();
     hideLoader();
  }
 
+ function getOmMig(data){
+    console.log("om mig data is: ", data);
+ }
 
  function submitForm(){  
  const form = document.querySelector("form");
-     form.addEventListener("submit", (e)=>{
-         e.preventDefault();
-         document.querySelector("#name").value = " ";
-         document.querySelector("#firma").value = " ";
-         document.querySelector("#email").value = " ";
-         document.querySelector("#message").value = " ";
- 
-         if(document.querySelector("#stilling")){
-            document.querySelector("#stilling").value = " "; 
-         }
-         displayPopup();
-     })
+
+ if(form !== null){
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        document.querySelector("#name").value = " ";
+        document.querySelector("#firma").value = " ";
+        document.querySelector("#email").value = " ";
+        document.querySelector("#message").value = " ";
+
+        if(document.querySelector("#stilling")){
+           document.querySelector("#stilling").value = " "; 
+        }
+        displayPopup();
+    })
+ }
+
  }
 
  function displayPopup(){
